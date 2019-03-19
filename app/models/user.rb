46b1ccd306eb9a055381fe3e9676c8b8558df2d6
Jeremy <jeremyroy2005@hotmail.com>
2019-03-20 02:47:05 +0900

    add foundation for password reset, will revisit later

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,4 +59,9 @@ class User < ApplicationRecord
       if: :require_password?
   }
 
+  def deliver_password_reset_instructions!
+    reset_perishable_token!
+    PasswordResetMailer.reset_email(self).deliver_now
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :update]
   resources :user_sessions, only: [:create, :destroy]
 
+  resources :password_resets, only: [:new, :create, :edit, :update]
+
   resources :conversations do
     resources :messages
   end
-  
+
   delete '/sign_out', to: 'user_sessions#destroy', as: :sign_out
   get '/sign_in', to: 'user_sessions#new', as: :sign_in
 
