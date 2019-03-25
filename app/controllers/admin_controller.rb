def8d70070fdbcc1a5f660abc0af5d7c74c2e913
Jeremy <jeremyroy2005@hotmail.com>
2019-03-26 01:33:11 +0900

    Add some stats to admin dashboard, small css adjustment

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,6 +5,19 @@ class AdminController < ApplicationController
       redirect_to root_path
     else
       @all_users = User.all
+
+      @ytd_signups = User.count
+      @ytd_visits = User.sum(:login_count)
+      @ytd_chats = Conversation.count
+
+      con = Conversation.all
+      total = 0
+      con.each do |c| #want to find a better way to do this, sqlite in dev and postgresql in prod makes this difficult
+        total += c.end_time - c.start_time
+      end
+      ytd_avg = total / @ytd_chats
+      @ytd_avg_length = "%s : %s : %s" % [(ytd_avg / 3600).round.to_s.rjust(2, '0'), (ytd_avg / 60).round.to_s.rjust(2, '0'), (ytd_avg % 60).round.to_s.rjust(2, '0')]
+
     end
   end
 
