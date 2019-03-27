c4bc9e001dff40669a29f85388d3895c0f15e428
Jeremy <jeremyroy2005@hotmail.com>
2019-03-28 08:56:12 +0900

    Integrate Dialogflow NLP

--- app/controllers/beardbot_controller.rb
+++ app/controllers/beardbot_controller.rb
@@ -3,7 +3,24 @@ class BeardbotController < ActionController::Base
 
   def chat
     fallbacks = ["Sorry, I don't know enough to respond to that.","Sorry, I'm not really sure about that.","Hmm, that's not familiar to me.","I don't think most dwarves know about that."]
-    reaction = BEARDBOT.get_reaction(params[:query])
-    render json: { response: reaction.present? ? reaction : fallbacks.sample }
+    accessToken = ENV['ACCESS_TOKEN'];
+		baseUrl = "https://api.api.ai/v1/";
+
+    require 'net/http'
+    require 'json'
+    begin
+        uri = URI(baseUrl + "query?v=20190101")
+        http = Net::HTTP.new(uri.host, uri.port)
+        http.use_ssl = true
+        req = Net::HTTP::Post.new(uri.path, {'Content-Type' =>'application/json',
+          'Authorization' => "Bearer " + accessToken})
+        req.body = {"query" => params[:query], "lang" => "en", "sessionId" => "1234asdf"}.to_json
+        res = http.request(req)
+        reaction = JSON.parse(res.body)
+
+        render json: { response: reaction.present? ? reaction["result"]["speech"] : fallbacks.sample }
+    rescue => e
+        puts "failed #{e}"
+    end
   end
 end
